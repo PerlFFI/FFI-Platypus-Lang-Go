2d06ae5c85c1d3a7e0cf6bd1b33597993f6cf220
Graham Ollis <plicease@cpan.org>
2018-08-29 21:40:39 -0400

    Initial commit

---
+++ lib/FFI/Platypus/Lang/Go.pm
@@ -0,0 +1,10 @@
+package FFI::Platypus::Lang::Go;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Documentation and tools for using Platypus with Go
+# VERSION
+
+1;
---
+++ t/ffi_platypus_lang_go.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use FFI::Platypus::Lang::Go;
+
+ok 1, 'todo';
+
+done_testing
